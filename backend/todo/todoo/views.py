81b3ca8c96ca771727eaf0d7fdb6f50361c5ab83
Cindy Pua <cindyqi0511@gmail.com>
2024-08-23 07:44:14 +0800

    api-update

--- backend/todo/todoo/views.py
+++ backend/todo/todoo/views.py
@@ -5,21 +5,34 @@ from .serializers import TaskSerializer
 from .models import Task
 
 # Create your views here.
-@api_view(['GET'])
-def taskList(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many = True)
-    return Response(serializer.data)
+
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'List' : '/task-list/',
-        'Detail View' : '/task-detail/<str:pk>/',
-        'Create' : '/task-create/',
-        'Update' : '/task-update/<str:pk>/',
-        'Delete' : '/task-delete/<str:pk>/',
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+    except Task.DoesNotExist:
+        return Response({'error': 'Task not found'}, status=404)
+
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
 @api_view(['POST'])
 def taskCreate(request):
     serializer = TaskSerializer(data=request.data)
@@ -27,6 +40,24 @@ def taskCreate(request):
         serializer.save()
     return Response(serializer.data)
 
-#request -> response
-#request handler
-#action
+@api_view(['PUT'])
+def taskUpdate(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+    except Task.DoesNotExist:
+        return Response({'error': 'Task not found'}, status=404)
+
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+    except Task.DoesNotExist:
+        return Response({'error': 'Task not found'}, status=404)
+
+    task.delete()
+    return Response({'message': 'Task successfully deleted'})
